ccba9c85a56abdfbf0165aad52b1bdc5aaadc46a
Mauro Birattari <mbiro@ulb.ac.be>
2004-02-07 00:00:00 +0000

    version 0.1-45

---
+++ inst/slave/srace.R
@@ -0,0 +1,41 @@
+# ---------------------------------------- -*- mode: r; mode: font-lock -*- #
+# srace.R                                                   Slave R process #
+# ------------------------------------------------------------------------- #
+                                                                             
+# ========================================================================= #
+# Racing methods for the selection of the best                              #
+# ------------------------------------------------------------------------- #
+# Copyright (C) 2003 Mauro Birattari                                        #
+# ========================================================================= #
+# This program is free software; you can redistribute it and/or modify it   #
+# under the terms of the GNU General Public License as published by the     #
+# Free Software Foundation; either version 2 of the License, or (at your    #
+# option) any later version.                                                #
+#                                                                           #
+# This program is distributed in the hope that it will be useful, but       #
+# WITHOUT ANY WARRANTY; without even the implied warranty of                #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         #
+# General Public License for more details.                                  #
+#                                                                           #
+# You should have received a copy of the GNU General Public License along   #
+# with this program; if not, write to the Free Software Foundation, Inc.,   #
+# 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.                  #
+# ========================================================================= #
+
+# ========================================================================= #
+# Mauro BIRATTARI                                                           #
+# IRIDIA - ULB, CP 194/6                                                    #
+# Av. F. D. Roosevelt 50                                    mbiro@ulb.ac.be #
+# 1050 Brussels, Belgium                     http://iridia.ulb.ac.be/~mbiro #
+# ========================================================================= #
+
+# $Id: srace.R,v 1.10 2003/09/29 09:38:29 mbiro Exp $ #
+
+library(race)
+
+# Start slave
+.race.slave()
+
+# Exit
+.PVM.exit()
+quit(save="no")
